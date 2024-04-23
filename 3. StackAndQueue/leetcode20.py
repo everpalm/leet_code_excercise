7beb37bbeaeec15a95659fc903215a04cc320ff9
Jaron Cheng <everpalm@ms58.url.com.tw>
2024-04-23 18:15:41 +0800

    Problem: Valid parentheses

---
+++ 3. StackAndQueue/leetcode20.py
@@ -0,0 +1,67 @@
+'''
+20. Valid Parentheses
+Attempted
+Easy
+
+Topics
+Companies
+
+Hint
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+'''
+class Solution(object):
+    def is_valid_parentheses(self, s):
+        # Dictionary to match the corresponding brackets (A list of close brackets)
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        # Stack to keep track of opening brackets
+        stack = []
+        
+        for char in s:
+            if char in bracket_map: # Close bracket
+                # If the stack is empty or the top of the stack does not match the closing bracket
+                if not stack or stack[-1] != bracket_map[char]:
+                    return False
+                print('pop stack = ', stack)
+                stack.pop()  # Pop the matching opening bracket
+            else: # Open bracket
+                stack.append(char)  # Push opening bracket onto stack
+                print('append stack = ', stack)
+        
+        # If stack is empty, all brackets are matched correctly
+        return not stack
+
+my_solution = Solution()
+# Example calls to test the function (commented out)
+print(my_solution.is_valid_parentheses("()"))         # Output: True
+print(my_solution.is_valid_parentheses("()[]{}"))     # Output: True
+print(my_solution.is_valid_parentheses("(]"))         # Output: False
+print(my_solution.is_valid_parentheses("([)]"))       # Output: False
+print(my_solution.is_valid_parentheses("{[()]}"))       # Output: True
+print(my_solution.is_valid_parentheses(""))       # Output: True
+# print(my_solution.is_valid_parentheses("abcd"))       # Output: True
---
+++ 4. HashTable/leetcode128.py
@@ -0,0 +1,58 @@
+'''
+128. Longest Consecutive Sequence
+Medium
+
+Topics
+Companies
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+'''
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_set = set(nums)
+        max_length = 0
+        print('num_set = ', num_set)
+
+        for num in num_set:
+        # Check if it's the start of a sequence
+            if num - 1 not in num_set:
+                length = 1
+                current_num = num
+                print('current_num = ', current_num)
+                print('num = ', num)
+                # Count the length of the sequence
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    length += 1
+                # Compare max length with current max length
+                max_length = max(max_length, length)
+        return max_length   
+
+
+
+data = [100, 4, 200, 1, 3, 2]
+my_solution = Solution()
+print(my_solution.longestConsecutive(data))
