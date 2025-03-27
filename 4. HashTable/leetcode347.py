30547e02e2f16324f850574aa589e0fe3948b8d8
Jaron Cheng <everpalm@ms58.url.com.tw>
2025-03-27 09:36:25 +0800

    Style: Rename leetcode485

--- 4. HashTable/leetcode347.py
+++ 4. HashTable/leetcode347.py
@@ -45,6 +45,7 @@ class Solution(object):
             output.append((freq, num))
         output.sort(reverse=True)
         return [num for _, num in output[:k]]
+
     
     def min_heap(self, nums, k):
         # Create a min-heap for the top k frequent elements
