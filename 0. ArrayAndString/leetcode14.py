8e466c6589277f11cbc978fb99cdb8af9c58e6bd
Jaron Cheng <everpalm@ms58.url.com.tw>
2025-04-01 14:26:58 +0800

    Update

--- 0. ArrayAndString/leetcode14.py
+++ 0. ArrayAndString/leetcode14.py
@@ -30,10 +30,11 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-        
+
+        str_length = len(strs)
         prefix = strs[0]
 
-        for j in range(1, len(strs)):
+        for j in range(1, str_length):
             count = 0    
             length = min(len(strs[j]), len(prefix))
 
@@ -42,48 +43,22 @@ class Solution:
                     count += 1
                 else:
                     break
-
             prefix = strs[j][:count]
-
             if prefix == "":
                 break
-
         return prefix
     
-    def vertical_scanning(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-        
-        # 找到最短字串的長度
-        min_len = min(len(s) for s in strs)
-        
-        # 遍歷到最短長度
-        for i in range(min_len):
-            # 獲取第一個字串當前位置的字符
-            current_char = strs[0][i]
-            
-            # 檢查其他字串的相同位置
-            for s in strs[1:]:
-                if s[i] != current_char:
-                    return strs[0][:i]
-        
-        # 如果所有字符都匹配，返回最短字串
-        return strs[0][:min_len]
-
 
 sol = Solution()
 
 strs = ["flower","flow","flight"]
 
 print("Example 1 = ", sol.longestCommonPrefix(strs))  # Expected = "fl"
-print("Example 1-1 = ", sol.vertical_scanning(strs))
 
 strs = ["dog","racecar","car"]
 
 print("Example 2 = ", sol.longestCommonPrefix(strs))  # Expected = ""
-print("Example 2-1 = ", sol.vertical_scanning(strs))
 
 strs = ["ab","a"]
 
 print("Example 3 = ", sol.longestCommonPrefix(strs))  # Expected = "a"
-print("Example 3-1 = ", sol.vertical_scanning(strs))
