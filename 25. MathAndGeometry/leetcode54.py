b093f65b4cdc54fbce314800bb2e046ceddfbe02
Jaron Cheng <everpalm@ms58.url.com.tw>
2025-06-12 21:15:57 +0800

    Style: Comply with PEP8

--- 25. MathAndGeometry/leetcode54.py
+++ 25. MathAndGeometry/leetcode54.py
@@ -61,5 +61,6 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
     print(solution.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))  # [1,2,3,6,9,8,7,4,5]
-    print(solution.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))  # [1,2,3,4,8,12,11,10,9,5,6,7]
+    print(solution.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+    # [1,2,3,4,8,12,11,10,9,5,6,7]
     
